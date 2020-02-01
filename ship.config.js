e3fb3fb6abd28f43ed82411157c398cc9925accf
jeetiss <jeetiss@yandex.ru>
2020-02-01 21:02:06 +0300

    feat: add cowsay

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
+  afterPublish: ({ exec, version }) => {
+    exec(`cowsay ${version} released`)
+  },
   mergeStrategy: { toSameBranch: ["master"] }
 };
